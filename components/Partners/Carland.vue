8091a95c3dd7c361115fd1f819def11798522c85
Oscar Paricio <oscarparicio8@gmail.com>
2023-03-08 04:37:00 +0100

    CSS Logo Carland

--- components/Partners/Carland.vue
+++ components/Partners/Carland.vue
@@ -1,3 +1,9 @@
 <template>
-<img src="/img/Carland.webp" alt="" width="132px" height="65px">
+<img class="CarlandImg" src="/img/Carland.webp" alt="" width="132px" height="65px">
 </template>
+<style>
+.CarlandImg {
+    width: 132px;
+    height: 65px;
+}
+</style>
