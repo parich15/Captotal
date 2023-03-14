3508ba1616bc87920b532fb8a5281a6080cf8868
Oscar Paricio <oscarparicio8@gmail.com>
2023-03-14 22:31:09 +0100

    Estilos mejorados desplegable

--- components/Curso/Desplegable.vue
+++ components/Curso/Desplegable.vue
@@ -2,16 +2,17 @@
     <div>
         <div 
             @click="mostrar = !mostrar"
-            class="flex w-full p-2">
-            <h4 :class="mostrar ? 'text-orange-500': ''" class="font-titulo font-semibold text-gray-500 text-xl">{{ titulo }}</h4>
-            <svg v-if="!mostrar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-auto text-gray-500">
+            :class="mostrar ? 'border-orange-400' : ''"
+            class="flex w-full p-2 border transition-colors items-center group hover:border-orange-400 bg-white shadow-md">
+            <h4 :class="mostrar ? 'text-orange-500': ''" class="font-titulo font-semibold text-gray-500 text-xl transition group-hover:text-orange-500 ">{{ titulo }}</h4>
+            <svg v-if="!mostrar" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 ml-auto text-gray-500 transition group-hover:text-orange-500">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3.75v4.5m0-4.5h4.5m-4.5 0L9 9M3.75 20.25v-4.5m0 4.5h4.5m-4.5 0L9 15M20.25 3.75h-4.5m4.5 0v4.5m0-4.5L15 9m5.25 11.25h-4.5m4.5 0v-4.5m0 4.5L15 15" />
             </svg>
             <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" ml-auto text-orange-500 w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 9V4.5M9 9H4.5M9 9L3.75 3.75M9 15v4.5M9 15H4.5M9 15l-5.25 5.25M15 9h4.5M15 9V4.5M15 9l5.25-5.25M15 15h4.5M15 15v4.5m0-4.5l5.25 5.25" />
             </svg>
         </div>
-        <div v-if="mostrar" class="p-2 contenido font-texto text-lg">
+        <div v-if="mostrar" class="p-2 contenido font-texto text-lg border border-t-0 border-orange-400 bg-gray-50">
             <div v-html="contenido"></div>
         </div>
     </div>
--- pages/Curso/[Titulo].vue
+++ pages/Curso/[Titulo].vue
@@ -34,8 +34,9 @@
                     :Subtitulo="secciones[1]?.Content[1]?.item.Titulo"
                     :Contenido="secciones[1]?.Content[2]?.item.Texto"
                     ></Contenido>
-                    <ul v-if="curso.Desplegable" id="Desplegables" class=" list-none">
-                        <li v-for="desplegable in curso.Desplegable" >
+                    <ul v-if="curso.Desplegable" id="Desplegables" class="list-none lg:pt-2">
+                        <li class="my-5 lg:my-6"
+                        v-for="desplegable in curso.Desplegable" >
                             <Desplegable :titulo="desplegable.Titulo" :contenido="desplegable.Texto"></Desplegable>
                         </li>
                     </ul>
