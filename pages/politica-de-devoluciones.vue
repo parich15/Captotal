b98e8c34886bc901be53cbfe077afb992f634aeb
Oscar Paricio <oscarparicio8@gmail.com>
2023-04-14 21:08:33 +0200

    Pagina Devoluciones + Inhabilitamos boton hasta validacion TPV

---
+++ pages/politica-de-devoluciones.vue
@@ -0,0 +1,20 @@
+<template>
+    <main>
+        <section class="flex h-48 bg-orange-400 justify-center items-center relative z-20 shadow-xl shadow-slate-300">
+            <h1 class="text-xl md:text-3xl lg:text-5xl font-titulo font-semibold text-white tracking-wide">Politica de Devoluciones</h1>
+        </section>
+        <section id="Contenido" class="my-5 container mx-auto" v-for="contenido in secciones[0].Content" :key="contenido.id">
+            <div v-for="seccion in contenido" class="pt-10">
+                <div class="mx-2 font-texto text-base antialiased" v-html="seccion.Texto">
+                </div>
+            </div>        
+        </section>
+    </main>
+</template>
+
+<script setup>
+import { usePageData } from '~~/composables/usePageData';
+const {secciones, getPageSections } = usePageData();
+
+await getPageSections(13);
+</script>
