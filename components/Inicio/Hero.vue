0faa947ea300672415b52b8bf652cfaa3da45b2c
Oscar Paricio <oscarparicio8@gmail.com>
2023-06-17 18:13:54 +0200

    Imagenes bonificados home

--- components/Inicio/Hero.vue
+++ components/Inicio/Hero.vue
@@ -30,7 +30,7 @@
     </div>
   </div>
   <div class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full object-right" :src="`https://admin.captotal.com/assets/${Portada}`" alt="">
+    <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:h-full lg:w-full object-right" :src="`https://admin.captotal.com/assets/${Portada}`" alt="Imagen cursos CAP Bonificados">
   </div>
 </div>
 </section>
