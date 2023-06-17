6d5f3ab743a75a9c9658ffc43dd6a36ae655fe61
Oscar Paricio <oscarparicio8@gmail.com>
2023-06-17 18:18:06 +0200

    Alt en imagenes Home

--- components/Partners/Carland.vue
+++ components/Partners/Carland.vue
@@ -1,5 +1,5 @@
 <template>
-<img class="CarlandImg" src="/img/Carland.webp" alt="" width="132px" height="65px">
+<img class="CarlandImg" src="/img/Carland.webp" alt="Logo autoescuela Car Land" width="132px" height="65px">
 </template>
 <style>
 .CarlandImg {
