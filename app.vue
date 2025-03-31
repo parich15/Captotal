d158255e43e1761dea4fd920c083d00d90faf9a3
ggerardm <gerardmoreno@captotal.com>
2025-03-31 16:25:00 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,29 +1,17 @@
 <template>
-  <head>
-    <link
-      rel="preload"
-      as="image"
-      href="https://admin.captotal.com/assets/584d8674-f819-4853-ab1c-550f7099e04b"
-      type="image/webp"
-    />
-  </head>
-<template>
   <div>
     <NuxtLoadingIndicator color="#f97316" />
-    <Topbar></Topbar>
+    <Topbar />
     <navbar></navbar>
-        <NuxtPage :transition="{
-          name: 'page',
-          mode: 'out-in'
-        }"></NuxtPage>
-      <LayoutBannerCookies></LayoutBannerCookies>
-    <Footer></Footer>
+    <NuxtPage :transition="{ name: 'page', mode: 'out-in' }" />
+    <LayoutBannerCookies />
+    <Footer />
     <div id="teleportContent"></div>
   </div>
 </template>
 
 <script setup>
-import navbar from './components/layout/navbar.vue';
+import Navbar from './components/layout/navbar.vue';
 import Topbar from './components/layout/topbar.vue';
 import Footer from './components/layout/footer.vue';
 </script>
@@ -31,8 +19,9 @@ import Footer from './components/layout/footer.vue';
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all .5s;
+  transition: all 0.5s;
 }
+
 .page-enter-from,
 .page-leave-to {
   opacity: 0;
